8baad28c53955a65bd0781d932d52d9a7c7bb746
Mohamed Nehad <mohammednehad550@gmail.com>
2022-06-11 11:48:34 +0200

    fix: Export api types

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,14 @@
 import pinsCRUD from "./pinsCRUD";
 import sequenceCRUD from "./sequenceCRUD";
+import { SequenceData, Pin, ScheduleData } from "./utils";
 
-export { pinsCRUD, sequenceCRUD }
+
+export {
+    pinsCRUD,
+    sequenceCRUD,
+}
+export type {
+    ScheduleData,
+    Pin,
+    SequenceData,
+}
