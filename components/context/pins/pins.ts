b402782d6dad495cffc76828e5259a47ab132ece
Mohamed Nehad <mohammednehad550@gmail.com>
2022-07-02 14:14:15 +0200

    fix: Fix Pin CRUDContext

--- components/context/pins/pins.ts
+++ components/context/pins/pins.ts
@@ -1,10 +1,11 @@
 import { createContext, useContext, useState } from "react";
-import { Pin, pinsCRUD } from "../../../api";
+import { pinsCRUD } from "../../../api";
+import { PinDbType } from "../../../Scheduler/src/db";
 import { CRUDContext, initCRUDContext } from "../utils";
 
 
-export const pinsContext = createContext<CRUDContext<Pin> | undefined>(undefined)
+export const pinsContext = createContext<CRUDContext<PinDbType['channel'], PinDbType> | undefined>(undefined)
 
-export const initPinsContext = () => initCRUDContext(pinsCRUD)
+export const initPinsContext = () => initCRUDContext(pinsCRUD, (p => p.channel))
 
 export const usePins = () => useContext(pinsContext)
