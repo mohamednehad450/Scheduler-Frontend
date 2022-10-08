0e7924109897958eca14e2bd9ae868308ed79460
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-08 21:10:21 +0200

    fix: Remove confirm from LoadingButton

--- components/common/LoadingButton.tsx
+++ components/common/LoadingButton.tsx
@@ -1,56 +1,18 @@
-import { Button, LoadingOverlay, ButtonProps, Modal, Group, Text, Divider, Space } from "@mantine/core"
+import { Button, LoadingOverlay, ButtonProps } from "@mantine/core"
 import { FC, PropsWithChildren, useState } from "react"
 
 
 const LoadingButton: FC<PropsWithChildren<ButtonProps<any> & { onClick?: (onDone: () => void) => void, confirm?: boolean }>> = (props) => {
     const [loading, setLoading] = useState(false)
-    const [confirmPrompt, setConfirmPrompt] = useState(false)
     return (
-        <>
-            <Button {...props} onClick={() => {
-                if (!props.onClick) return
-                if (props.confirm) {
-                    setConfirmPrompt(true)
-                }
-                else {
-                    setLoading(true)
-                    props.onClick(() => setLoading(false))
-                }
-            }}>
-                {props.children}
-                <LoadingOverlay visible={loading} />
-            </Button>
-            {props.confirm && props.onClick && (
-                <Modal
-                    opened={confirmPrompt}
-                    onClose={() => setConfirmPrompt(false)}
-                    title="Are you sure?"
-                    centered
-                >
-                    <Group position="right" mt="lg">
-                        <Button
-                            color={'gray'}
-                            onClick={() => {
-                                setLoading(() => false)
-                                setConfirmPrompt(() => false)
-                            }}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            color={'red'}
-                            onClick={() => {
-                                setConfirmPrompt(false)
-                                setLoading(true)
-                                props.onClick && props.onClick(() => setLoading(false))
-                            }}
-                        >
-                            Confirm
-                        </Button>
-                    </Group>
-                </Modal>
-            )}
-        </>
+        <Button {...props} onClick={() => {
+            if (!props.onClick) return
+            setLoading(true)
+            props.onClick(() => setLoading(false))
+        }}>
+            {props.children}
+            <LoadingOverlay visible={loading} />
+        </Button>
     )
 }
 
