7384050da2b0860a9387a5a47483dab34af7b392
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-09 16:38:48 +0200

    feat: Add NewSequence to prompt context

--- components/context/prompt/prompt.tsx
+++ components/context/prompt/prompt.tsx
@@ -1,8 +1,11 @@
 import { createContext, ReactNode, useContext, useState } from "react"
+import { SequenceDBType } from "../../../Scheduler/src/db"
 import { ConfirmModal } from "../../common"
+import { NewSequence } from "../../sequences"
 
 type PromptContext = {
     confirm: (onDone: (confirmed: boolean) => void, message?: string) => void
+    newSequence: (onDone: (newSeq: SequenceDBType) => void, initialSequence?: SequenceDBType) => void
 }
 
 const promptContext = createContext<PromptContext | undefined>(undefined)
@@ -12,30 +15,47 @@ const usePrompt = () => useContext(promptContext)
 
 const usePromptContext = (): { context: PromptContext, modal: ReactNode } => {
 
-    const [opened, setOpened] = useState(false)
+    const [openedConfirm, setOpenedConfirm] = useState(false)
     const [confirm, setConfirm] = useState<{ message?: string, onDone: (confirmed: boolean) => void }>()
 
+    const [openedNewSeq, setOpenedNewSeq] = useState(false)
+    const [newSeq, setNewSeq] = useState<{ initialSequence?: SequenceDBType, onDone: (seq: SequenceDBType) => void }>()
 
     return {
         context: {
             confirm: (onDone: (confirmed: boolean) => void, message?: string) => {
-                setOpened(true)
+                setOpenedConfirm(true)
                 setConfirm({
                     message,
                     onDone,
                 })
             },
+            newSequence: (onDone, initialSequence) => {
+                setOpenedNewSeq(true)
+                setNewSeq({ onDone, initialSequence })
+            }
         },
         modal: (
-            <ConfirmModal
-                opened={opened}
-                onDone={(confirmed) => {
-                    setOpened(false)
-                    confirm?.onDone(confirmed)
-                    setConfirm(undefined)
-                }}
-                message={confirm?.message}
-            />
+            <>
+                <ConfirmModal
+                    opened={openedConfirm}
+                    onDone={(confirmed) => {
+                        setOpenedConfirm(false)
+                        confirm?.onDone(confirmed)
+                        setConfirm(undefined)
+                    }}
+                    message={confirm?.message}
+                />
+                <NewSequence
+                    onClose={(seq) => {
+                        setOpenedNewSeq(false)
+                        seq && newSeq?.onDone(seq)
+                        setNewSeq(undefined)
+                    }}
+                    opened={openedNewSeq}
+                    initialSequence={newSeq?.initialSequence}
+                />
+            </>
         )
     }
 }
