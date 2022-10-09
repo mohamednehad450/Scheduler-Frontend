eb9c3f36533ff543d2b78c5e198d283badd17f46
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-09 16:38:25 +0200

    fix: Refactor NewSequence

--- components/sequences/NewSequence.tsx
+++ components/sequences/NewSequence.tsx
@@ -1,11 +1,8 @@
-import { Button, Divider, Group, Modal, ScrollArea, Stepper, Switch, Text, TextInput, ThemeIcon } from "@mantine/core";
-import { useRouter } from "next/router";
+import { Button, Divider, Group, Modal, ScrollArea, Switch, TextInput } from "@mantine/core";
 import { FC, useEffect, useState } from "react";
-import { CircleCheck, } from "tabler-icons-react";
-import { cronCRUD, cronSequence, pinsCRUD, sequenceCRUD } from "../../api";
-import { CronDbType, PinDbType, SequenceDBType } from "../../Scheduler/src/db";
+import { pinsCRUD, sequenceCRUD } from "../../api";
+import { PinDbType, SequenceDBType } from "../../Scheduler/src/db";
 import { LoadingButton } from "../common";
-import CronSelect from "./CronSelect";
 import OrdersInput, { OrderInput } from "./OrdersInput";
 
 
@@ -15,49 +12,43 @@ type SequenceInput = {
     orders: OrderInput[],
 }
 
-
-const NewSequence: FC<{
+interface NewSequenceProps {
     opened: boolean,
     onClose: (newSeq?: SequenceDBType) => void,
     initialSequence?: SequenceDBType
-}> = ({ onClose, initialSequence, opened }) => {
+}
 
-    const [crons, setCrons] = useState<CronDbType[]>([])
-    const [pins, setPins] = useState<PinDbType[]>([])
 
-    useEffect(() => {
-        cronCRUD.list()
-            .then(d => setCrons(d.data))
-        pinsCRUD.list()
-            .then(d => setPins(d.data))
-    }, [])
-
-    const router = useRouter()
-
+const NewSequence: FC<NewSequenceProps> = ({ onClose, initialSequence, opened }) => {
 
     const [sequence, setSequence] = useState<SequenceInput>({
         name: initialSequence?.name || '',
         active: initialSequence?.active || false,
         orders: initialSequence?.orders.map(({ offset, duration, channel }) => ({ offset, duration, channel })) || []
     })
-
-    const [newSequence, setNewSequence] = useState<SequenceDBType>()
-
-
-    const [triggers, setTriggers] = useState<{
-        seqId?: number,
-        cronsIds: number[]
-    }>({
-        seqId: initialSequence?.id || undefined,
-        cronsIds: initialSequence?.CronSequence.map(c => c.cron.id) || []
-    })
-
-    const [step, setStep] = useState(0)
-
     const [error, setError] = useState({
         name: '',
         orders: ''
     })
+    const [pins, setPins] = useState<PinDbType[]>([])
+
+
+    useEffect(() => {
+        if (opened) {
+            pinsCRUD.list()
+                .then(d => setPins(d.data))
+            setSequence({
+                name: initialSequence?.name || '',
+                active: initialSequence?.active || false,
+                orders: initialSequence?.orders.map(({ offset, duration, channel }) => ({ offset, duration, channel })) || []
+            })
+            setError({
+                name: '',
+                orders: '',
+            })
+        }
+    }, [opened])
+
 
     useEffect(() => {
         setError({
@@ -65,7 +56,6 @@ const NewSequence: FC<{
             orders: ''
         })
     }, [sequence])
-
 
 
     return (
@@ -77,136 +67,76 @@ const NewSequence: FC<{
             title={initialSequence ? `Edit ${initialSequence.name}` : "Add New Sequence"}
         >
             <Divider pt="md" />
-            <Stepper
-                styles={{ content: { minHeight: '12rem' }, steps: { padding: `0 2rem`, } }}
-                active={step}
-                breakpoint="xs"
-                size="sm"
+            <ScrollArea
+                styles={{ viewport: { height: '24rem' } }}
             >
-                <Stepper.Step label="First step" description={initialSequence ? `Edit ${initialSequence.name}` : "Create new Sequence"}>
-                    <ScrollArea
-                        styles={{ viewport: { height: '24rem' } }}
-                    >
-                        <Group p="xs">
-                            <TextInput
-                                required
-                                value={sequence.name}
-                                onChange={(e) => setSequence(s => ({ ...s, name: e.target.value }))}
-                                label="Name"
-                                error={error.name}
-                            />
-                            <Switch
-                                px="md"
-                                mt="xl"
-                                checked={sequence.active}
-                                onChange={(e) => setSequence(s => ({ ...s, active: e.target.checked }))}
-                                label="Active"
-                            />
-                        </Group>
-                        <Group p="xs">
-
-                        </Group>
-                        <OrdersInput
-                            error={error.orders}
-                            orders={sequence.orders}
-                            pins={pins}
-                            onChange={orders => setSequence(s => ({ ...s, orders }))}
-                        />
-                    </ScrollArea>
-                </Stepper.Step>
-                <Stepper.Step label="Second step" description="Add Triggers" >
-                    <CronSelect
-                        crons={crons}
-                        value={triggers.cronsIds}
-                        onChange={cronsIds => setTriggers(t => ({ ...t, cronsIds }))}
+                <Group p="xs">
+                    <TextInput
+                        required
+                        value={sequence.name}
+                        onChange={(e) => setSequence(s => ({ ...s, name: e.target.value }))}
+                        label="Name"
+                        error={error.name}
                     />
-                </Stepper.Step>
-                <Stepper.Completed>
-                    <div style={{ minHeight: '11rem', display: 'grid', placeItems: 'center' }}>
-                        <ThemeIcon size={96} variant="outline" style={{ borderWidth: 0 }}  >
-                            <CircleCheck size={96} />
-                        </ThemeIcon>
-                        <Text>All Done.</Text>
-                    </div>
-                </Stepper.Completed>
-            </Stepper>
+                    <Switch
+                        px="md"
+                        mt="xl"
+                        checked={sequence.active}
+                        onChange={(e) => setSequence(s => ({ ...s, active: e.target.checked }))}
+                        label="Active"
+                    />
+                </Group>
+                <Group p="xs">
+
+                </Group>
+                <OrdersInput
+                    error={error.orders}
+                    orders={sequence.orders}
+                    pins={pins}
+                    onChange={orders => setSequence(s => ({ ...s, orders }))}
+                />
+            </ScrollArea>
 
             <Group position="right">
                 <Button
                     styles={{ root: { minWidth: '6rem' } }}
                     m="sm"
                     variant='subtle'
-                    onClick={[
-                        () => onClose(),
-                        () => setStep(2),
-                        () => setStep(1),
-                    ][step]}
+                    onClick={() => onClose()}
                 >
-                    {["Cancel", "Skip", "Back"][step]}
+                    Cancel
                 </Button>
                 <LoadingButton
                     styles={{ root: { minWidth: '6rem' } }}
                     m='sm'
-                    onClick={[
-                        (onDone: () => void) => {
-                            let err = false
-                            if (!sequence.name) {
-                                setError(e => ({ ...e, name: 'You must provide a name.' }))
-                                err = true
-                            }
-
-                            if (sequence.orders.length < 1) {
-                                setError(e => ({ ...e, orders: 'You must add at least 1 channel' }))
-                                err = true
-                            }
-                            if (err) {
-                                onDone()
-                                return
-                            }
-                            initialSequence ?
-                                sequenceCRUD.update(initialSequence.id, sequence)
-                                    .then(({ data: newSeq }) => {
-                                        setStep(1)
-                                        setTriggers({ seqId: newSeq.id, cronsIds: newSeq.CronSequence.map(c => c.cron.id) })
-                                        setNewSequence(newSeq)
-                                        onDone()
-                                    })
-                                    .catch(err => {
-                                        // TODO
-                                        onDone()
-                                        return
-
-                                    })
-                                : sequenceCRUD.add(sequence)
-                                    .then(({ data: newSeq }) => {
-                                        setStep(1)
-                                        setTriggers({ seqId: newSeq.id, cronsIds: [] })
-                                        setNewSequence(newSeq)
-                                        onDone()
-                                    })
-                                    .catch(err => {
-                                        // TODO
-                                        onDone()
-                                        return
-                                    })
-                        },
-                        (onDone: () => void) => {
-                            triggers.seqId && cronSequence.linkSequencePromise(triggers.seqId, triggers.cronsIds)
-                                .then(() => {
-                                    setStep(2)
-                                    onDone()
-                                })
-                                .catch(err => {
-                                    // TODO
-                                    onDone()
-                                })
-                        },
-                        (onDone: () => void) => initialSequence ?
-                            onClose(newSequence) :
-                            router.push('/sequences/' + triggers.seqId)
-                    ][step]}
+                    onClick={(onDone) => {
+                        let err = false
+                        if (!sequence.name) {
+                            setError(e => ({ ...e, name: 'You must provide a name.' }))
+                            err = true
+                        }
+                        if (sequence.orders.length < 1) {
+                            setError(e => ({ ...e, orders: 'You must add at least 1 channel' }))
+                            err = true
+                        }
+                        if (err) {
+                            onDone()
+                            return
+                        }
+                        const func = initialSequence ?
+                            sequenceCRUD.update(initialSequence.id, sequence) :
+                            sequenceCRUD.add(sequence)
+                        func
+                            .then(({ data: newSeq }) => {
+                                onClose(newSeq)
+                            })
+                            .catch(err => {
+                                // TODO
+                            })
+                            .finally(() => onDone())
+                    }}
                 >
-                    {["Next", "Next", "Done"][step]}
+                    Submit
                 </LoadingButton>
             </Group>
         </Modal>
