aa1800c91785b63bea515e63a18ea6b875310f61
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-26 00:59:58 +0200

    fix: Fix SequenceTrigger layout

--- components/sequences/SequenceTriggers.tsx
+++ components/sequences/SequenceTriggers.tsx
@@ -1,14 +1,11 @@
-import { Accordion, Center, Container, Divider, Group, ScrollArea, Table, Text } from "@mantine/core";
+import { Accordion, AccordionItem, Container, Divider, Group, ScrollArea, Tab, Table, Tabs, Text } from "@mantine/core";
 import { FC } from "react";
 import { SequenceDBType } from "../../Scheduler/src/db";
-import { CronJob } from 'cron'
 import { nextCronDates } from "../common";
 
 interface SequenceTriggersProps {
     cronTriggers: SequenceDBType['CronSequence']
 }
-
-
 
 
 const SequenceTriggers: FC<SequenceTriggersProps> = ({ cronTriggers }) => {
@@ -20,38 +17,37 @@ const SequenceTriggers: FC<SequenceTriggersProps> = ({ cronTriggers }) => {
             </Group>
             <Divider />
             <ScrollArea pt="xs" styles={{ root: { flex: 1 } }}>
-                <Accordion offsetIcon={!cronTriggers.length}>
-                    <Accordion.Item label="Cron Triggers"  >
-                        {cronTriggers.length ? (
-                            <Accordion offsetIcon={false}  >
-                                {cronTriggers.map(({ cron }) => (
-                                    <Accordion.Item key={cron.id} iconPosition="right" label={cron.label} >
-                                        <Table striped highlightOnHover>
-                                            <thead>
-                                                <tr>
-                                                    <th>Date</th>
-                                                    <th>Time</th>
+                <Tabs>
+                    <Tab label="Cron" >
+                        <Accordion>
+                            {cronTriggers.map(({ cron }) => (
+                                <AccordionItem label={cron.label} iconPosition="right" >
+                                    <Table striped highlightOnHover>
+                                        <thead>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Time</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {nextCronDates(cron.cron, 5).map(d => (
+                                                <tr key={d.toString()}>
+                                                    <td>
+                                                        {d.toDateString()}
+                                                    </td>
+                                                    <td>
+                                                        {d.toLocaleTimeString()}
+                                                    </td>
                                                 </tr>
-                                            </thead>
-                                            <tbody>
-                                                {nextCronDates(cron.cron, 5).map(d => (
-                                                    <tr key={d.toString()}>
-                                                        <td>
-                                                            {d.toDateString()}
-                                                        </td>
-                                                        <td>
-                                                            {d.toLocaleTimeString()}
-                                                        </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </Table>
-                                    </Accordion.Item>
-                                ))}
-                            </Accordion>
-                        ) : (<Text>No cron triggers added</Text>)}
-                    </Accordion.Item>
-                </Accordion>
+                                            ))}
+                                        </tbody>
+                                    </Table>
+                                </AccordionItem>
+                            ))}
+                        </Accordion>
+                    </Tab>
+                    <Tab label="Sensors" disabled></Tab>
+                </Tabs>
             </ScrollArea >
         </Container >
     )
