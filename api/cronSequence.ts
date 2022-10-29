9282bfc450cfdf571d0d9c772b2d2a00963304c7
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-29 11:25:54 +0200

    feat: Refactor CronSequence api

--- api/cronSequence.ts
+++ api/cronSequence.ts
@@ -1,32 +1,21 @@
 import axios from "axios"
 import { CronDbType, SequenceDBType } from "../Scheduler/src/db"
 
-type Callback<T> = (err: unknown, val?: T) => void
-
 const url = '/api/link'
 
-class cronSequence {
 
-    linkCronPromise = async (id: CronDbType['id'], sequencesIds: SequenceDBType['id'][]): Promise<CronDbType> => {
-        return (await axios.post<CronDbType>(url + "/cron/" + id, sequencesIds)).data
-    }
-    linkCron = (id: CronDbType['id'], sequencesIds: SequenceDBType['id'][], cb: Callback<CronDbType>) => {
-        this.linkCronPromise(id, sequencesIds)
-            .then(cron => cb(null, cron))
-            .catch(err => cb(err))
-    }
 
-    linkSequencePromise = async (id: SequenceDBType['id'], cronsIds: CronDbType['id'][]): Promise<SequenceDBType> => {
-        return (await axios.post<SequenceDBType>(url + "/sequence/" + id, cronsIds)).data
 
-    }
-    linkSequence = (id: SequenceDBType['id'], cronsIds: CronDbType['id'][], cb: Callback<SequenceDBType>) => {
-        this.linkSequencePromise(id, cronsIds)
-            .then(sequence => cb(sequence))
-            .catch(err => cb(err))
-    }
+class CronSequence {
+
+    linkCron = (id: CronDbType['id'], sequencesIds: SequenceDBType['id'][], token: string) =>
+        axios.post<CronDbType>(url + "/cron/" + id, sequencesIds, { params: { token } })
+
+    linkSequence = (id: SequenceDBType['id'], cronsIds: CronDbType['id'][], token: string) =>
+        axios.post<SequenceDBType>(url + "/sequence/" + id, cronsIds, { params: { token } })
+
 }
 
 
-export default new cronSequence
+export default new CronSequence
 
