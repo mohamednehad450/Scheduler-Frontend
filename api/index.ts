5b4b34ae325c54481619238e8fc9dc536c222296
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-29 11:22:58 +0200

    feat: Refactor api utils

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,18 @@
+import auth from "./auth";
 import cronCRUD from "./cronCRUD";
 import cronSequence from "./cronSequence";
 import pinsCRUD from "./pinsCRUD";
 import sequenceCRUD from "./sequenceCRUD";
 import sequenceEvents from "./sequenceEvents";
-
-
+import { CRUD, Events } from "./utils";
 
 export {
     pinsCRUD,
     sequenceCRUD,
     sequenceEvents,
     cronCRUD,
-    cronSequence
+    cronSequence,
+    auth,
+    CRUD,
+    Events,
 }
--- api/utils.ts
+++ api/utils.ts
@@ -1,17 +1,15 @@
 import axios from "axios"
 
-type Callback<T> = (err: unknown, val?: T) => void
 
 interface CRUDClass<K, T> {
-    get: (id: K) => Promise<{ data: T }>
-    add: (obj: any) => Promise<{ data: T }>
-    set: (id: K, obj: any) => Promise<{ data: T }>
-    update: (id: K, obj: any) => Promise<{ data: T }>
-    remove: (id: K) => Promise<{}>
-    list: () => Promise<{ data: T[] }>
+    get: (id: K, token: string) => Promise<{ data: T }>
+    add: (obj: any, token: string) => Promise<{ data: T }>
+    set: (id: K, obj: any, token: string) => Promise<{ data: T }>
+    update: (id: K, obj: any, token: string) => Promise<{ data: T }>
+    remove: (id: K, token: string) => Promise<{}>
+    list: (token: string) => Promise<{ data: T[] }>
 }
 // TODO: Add pagination 
-// TODO: Add auth
 class CRUD<K, T> implements CRUDClass<K, T>{
 
     url: string
@@ -19,46 +17,34 @@ class CRUD<K, T> implements CRUDClass<K, T>{
     constructor(url: string) {
         this.url = url
     }
-    get = (id: K) => axios.get<T>(this.url + '/' + id);
-    add = (obj: any) => axios.post<T>(this.url, obj);
-    set = (id: K, obj: any) => axios.put<T>(this.url + "/" + id, obj);
-    update = async (id: K, obj: any) => axios.patch<T>(this.url + "/" + id, obj)
-    remove = (id: K) => axios.delete<void>(this.url + "/" + id);
-    list = () => axios.get<T[]>(this.url);
+    get = (id: K, token: string) => axios.get<T>(this.url + '/' + id, { params: { token } });
+    add = (obj: any, token: string) => axios.post<T>(this.url, obj, { params: { token } });
+    set = (id: K, obj: any, token: string) => axios.put<T>(this.url + "/" + id, obj, { params: { token } });
+    update = async (id: K, obj: any, token: string) => axios.patch<T>(this.url + "/" + id, obj, { params: { token } })
+    remove = (id: K, token: string) => axios.delete<void>(this.url + "/" + id, { params: { token } });
+    list = (token: string) => axios.get<T[]>(this.url, { params: { token } });
 }
 
 
-class Events<K, T>  {
+interface EventsClass<K, T> {
+    deleteAll: (token: string) => Promise<void>;
+    deleteById: (id: K, token: string) => Promise<void>;
+    listById: (id: K, token: string) => Promise<{ data: T[] }>
+    listAll: (token: string) => Promise<{ data: T[] }>
+}
+
+
+class Events<K, T> implements EventsClass<K, T>  {
     url: string
 
     constructor(url: string) {
         this.url = url
     }
 
-
-    deleteAllPromise = () => axios.delete<void>(this.url);
-    deleteAll = (cb: Callback<void>) => this.deleteAllPromise()
-        .then(() => cb(null))
-        .catch(err => cb(err))
-
-    deleteByIdPromise = (objId: any) => axios.delete(this.url + "/" + objId);
-    deleteById = (objId: any, cb: Callback<void>) => this.deleteByIdPromise(objId)
-        .then(() => cb(null))
-        .catch(err => cb(err));
-
-
-    listByIdPromise = (id: any) => axios.get(this.url + '/' + id);
-    listById = (id: any, cb: Callback<T[]>) => this.listByIdPromise(id)
-        .then(l => cb(null, l.data))
-        .catch(err => cb(err));
-
-
-    listAllPromise = () => axios.get(this.url);
-    listAll = (cb: Callback<T[]>) => this.listAllPromise()
-        .then(l => cb(null, l.data))
-        .catch(err => cb(err));
-
-
+    deleteAll = async (token: string) => axios.delete<void, void>(this.url, { params: { token } });
+    deleteById = (id: K, token: string) => axios.delete<void, void>(this.url + "/" + id, { params: { token } });
+    listById = (id: K, token: string) => axios.get<T[]>(this.url + '/' + id, { params: { token } });
+    listAll = (token: string) => axios.get<T[]>(this.url, { params: { token } });
 }
 
 export { CRUD, Events }
