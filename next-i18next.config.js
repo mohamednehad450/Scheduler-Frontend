d4e79808b5694597d079cd7f138fa07544d7be58
Mohamed Nehad <mohammednehad550@gmail.com>
2022-11-24 08:41:50 +0200

    feat: Add next-i18next

---
+++ next-i18next.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('next-i18next').UserConfig}
+ */
+
+module.exports = {
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "ar"],
+    },
+
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config')
+
+
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
   rewrites: async () => {
     return [
