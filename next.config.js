b0d3c9e8e4fd4db8349d7fcd8e6f75e0c93689f9
Mohamed Nehad <mohammednehad550@gmail.com>
2023-05-18 13:27:36 +0300

    feat: Update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require("./next-i18next.config");
-
 const nextConfig = {
-  i18n,
   reactStrictMode: true,
   staticPageGenerationTimeout: 120,
+  publicRuntimeConfig: {
+    i18n: {
+      languages: ["en", "ar"],
+      defaultLanguage: "en",
+      namespaces: ["common"],
+      defaultNamespace: "common",
+    },
+  },
 };
 
 module.exports = nextConfig;
